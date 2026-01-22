089df1923b3d676944cfc046cf00bd9456b01082
unknown <me.farazzafar@gmail.com>
2026-01-22 22:42:27 +0500

    addsite map xml file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.static("public"));
 
 
 
